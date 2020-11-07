5949600cd19e9722aaf50aba40bb353f2731ab9a
cjacobme <cjacobme@aol.com>
2020-11-07 20:58:34 +0100

    project created

---
+++ CJ  -  Experiments  -  Yaml/src/main/java/cj/software/experiments/yaml/Person.java
@@ -0,0 +1,11 @@
+package cj.software.experiments.yaml;
+
+import java.io.Serializable;
+
+public class Person
+		implements
+		Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+}
