1c549426b0c64450d722d949802b774d96101117
cjacobme <cjacobme@aol.com>
2020-11-07 21:00:26 +0100

    added name

--- CJ  -  Experiments  -  Yaml/src/main/java/cj/software/experiments/yaml/Person.java
+++ CJ  -  Experiments  -  Yaml/src/main/java/cj/software/experiments/yaml/Person.java
@@ -2,10 +2,22 @@ package cj.software.experiments.yaml;
 
 import java.io.Serializable;
 
-public class Person
+public abstract class Person
 		implements
 		Serializable
 {
 	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 
 }
